c8c3f5bcd7683c7372c80d3e436f48bc36705a62
Jazmin <jazminmattachavez@gmail.com>
2024-04-15 19:44:45 +0200

    Animacion del precharge page lista

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,15 @@
-console.log("Pollo")
+document.addEventListener("DOMContentLoaded", () => {
+    const imagenesGemas = document.querySelectorAll("img");
+    const arrayGemas = Array.from(imagenesGemas);
+
+    if (arrayGemas.length > 0) { // Verificar si hay imágenes disponibles
+        const randomIndex = Math.floor(Math.random() * arrayGemas.length);
+        const randomGem = arrayGemas.splice(randomIndex, 1)[0]; // Elimina y obtiene la imagen aleatoria
+
+        randomGem.classList.add("gem"); // Agregar la clase gem a la imagen aleatoria
+
+        arrayGemas.forEach((img) => {
+            img.classList.remove("gem"); // Quitar la clase gem de las otras imágenes
+        });
+    }
+});
