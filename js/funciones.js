a7a769e33337673962c6de05aea0424c15437919
Jazmin <jazminmattachavez@gmail.com>
2024-04-15 20:45:46 +0200

    Galeria hecha, falta mouseover

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,4 @@
+//Cada vez que se recargue aparezca una gema aleatoria
 document.addEventListener("DOMContentLoaded", () => {
     const imagenesGemas = document.querySelectorAll("img");
     const arrayGemas = Array.from(imagenesGemas);
@@ -12,4 +13,41 @@ document.addEventListener("DOMContentLoaded", () => {
             img.classList.remove("gem"); // Quitar la clase gem de las otras imágenes
         });
     }
+});
+//Galeria
+const enlaces = document.querySelectorAll(".galeria a");
+const modal = document.querySelector(".modal");
+const imgModal = document.querySelector(".modal img");
+const navegacionImg = document.querySelectorAll(".modal button");
+let imgActual = 0;
+
+//Enlace viene a ser como la i en un for
+enlaces.forEach((enlace, indice) => {
+	enlace.addEventListener("click", evento => {
+		evento.preventDefault();
+		//Guardamos la imagen donde estamos en una variable
+		imgActual = indice;
+		modal.classList.add("visible");
+		//Lee lo que tiene escrito href en enlace y ponselo a la img modal
+		imgModal.setAttribute("src", enlaces[imgActual].getAttribute("href"));
+	});
+});
+
+modal.addEventListener("click", () => {
+	modal.classList.remove("visible");
+})
+
+navegacionImg.forEach((flecha, i) => {
+	flecha.addEventListener("click", evento => {
+		evento.stopPropagation();
+		//modificar el valor de imgActual
+		if(i == 0){
+			//anterior
+			imgActual = imgActual > 0 ? imgActual - 1 : enlaces.length - 1;
+		}else{
+			//suma
+			imgActual = imgActual < enlaces.length - 1  ? imgActual + 1 : 0;
+		}
+		imgModal.setAttribute("src", enlaces[imgActual].getAttribute("href"));
+	});
 });
