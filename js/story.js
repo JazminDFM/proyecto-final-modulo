19e6b8916bd956c7dddb2c269f3f6f70f6531f30
Jazmin <jazminmattachavez@gmail.com>
2024-04-21 20:26:55 +0200

    Boton con su funcion chula hecho

--- js/story.js
+++ js/story.js
@@ -12,3 +12,46 @@ function desactivarShake(){
 
 cookie.addEventListener("mouseover", activarShake);
 cookie.addEventListener("mouseout", desactivarShake);
+
+
+let creandoConfeti = false;
+
+cookie.addEventListener("click", () => {
+    if (!creandoConfeti) {
+        creandoConfeti = true;
+        cookie.disabled = true; // Desactivar el botón temporalmente
+        
+        for(let i = 0; i < 100; i++) {
+            crearConfeti();
+        }
+        
+        setTimeout(() => {
+            creandoConfeti = false;
+            cookie.disabled = false; // Activar el botón después de un tiempo de espera
+            
+            // Ocultar las imágenes del confeti
+            const confetiElements = document.querySelectorAll(".confeti");
+            confetiElements.forEach(element => {
+                element.classList.add("hide-confeti");
+            });
+        }, 3000); // Esperar el tiempo de duración de la animación del confeti
+    }
+});
+
+function crearConfeti() {
+    const confeti = document.createElement("img");
+    confeti.src = "../img/cookie.png"; // Imagen de confeti
+    confeti.classList.add("confeti");
+    
+    // Posición aleatoria en la pantalla
+    confeti.style.left = Math.random() * window.innerWidth + 'px';
+    confeti.style.top = Math.random() * window.innerHeight + 'px';
+    
+    // Añadir el confeti al cuerpo del documento
+    document.body.appendChild(confeti);
+    
+    // Eliminar el confeti después de que termine la animación
+    confeti.addEventListener('animationend', function() {
+        confeti.remove();
+    });
+}
